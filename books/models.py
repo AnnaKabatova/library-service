cc8db50f29de8592ab1b8addfbf8f59446c5b608
Anna Kabatova <kabatova.anna@ukr.net>
2023-03-19 16:58:00 +0200

    Implemented CRUD for books

--- books/models.py
+++ books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class Enum(models.IntegerChoices):
+        HARD = 0
+        SOFT = 1
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=63)
+    cover = models.BooleanField(choices=Enum.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
+    out_of_books = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
+
+    def clean_inventory(self) -> None:
+        if self.inventory == 0:
+            self.out_of_books = True
--- library_service/urls.py
+++ library_service/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/library/", include("books.urls", namespace="books")),
+    path("api/library/", include("borrowing.urls", namespace="borrowings")),
+    path("users/", include("user.urls", namespace="users")),
 ]
